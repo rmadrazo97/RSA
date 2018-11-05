dbbeba761a6542517d67c8352d807d1be03b76f2
Jose Guzman <joseguzmanzamora@gmail.com>
2018-11-05 14:39:36 -0600

    backend inicial

--- rsa.js
+++ rsa.js
@@ -1,4 +1,44 @@
-// Selecting 2 primes.
-var p,q
+const primality = require("primality")
 
-// n = p*q -> first part of public key. 
+var prime_numbers = () => {
+    var start = false 
+    while(!start){
+        var numero = Math.floor(Math.random() * 10000) + 1000
+        if(primality(numero)){
+            start = true
+        }
+    }
+    return numero
+}
+
+var find_n = () => {
+    var num1 = prime_numbers()
+    var num2 = prime_numbers() 
+    var start = false
+    while(!start){
+        if(num1 == num2){
+            start = true
+        }else{
+            num2 = prime_numbers()
+        }
+    }
+    return num1 * num2
+}
+
+var gcd = (a,b) => {
+    var x = a
+    var y = b 
+    while(y != 0){
+        var r = x % y 
+        x = y 
+        y = r 
+    }
+    return x 
+}
+
+var lcm = (a,b) => {
+    return (a * b) / gcd(a,b)
+}
+
+console.log(gcd(123,276))
+console.log(lcm(123,276))
