36cba5d149909d6c20372b065a61c3b1ce66a2c3
Jose Guzman <joseguzmanzamora@gmail.com>
2018-11-05 15:11:02 -0600

    find e

--- rsa.js
+++ rsa.js
@@ -14,15 +14,18 @@ var prime_numbers = () => {
 var find_n = () => {
     var num1 = prime_numbers()
     var num2 = prime_numbers() 
+    console.log(num1)
+    console.log(num2)
     var start = false
     while(!start){
-        if(num1 == num2){
+        if(num1 != num2){
             start = true
         }else{
             num2 = prime_numbers()
         }
     }
-    return num1 * num2
+    var respuesta = [num1, num2, num1 * num2]
+    return respuesta
 }
 
 var gcd = (a,b) => {
@@ -40,5 +43,18 @@ var lcm = (a,b) => {
     return (a * b) / gcd(a,b)
 }
 
-console.log(gcd(123,276))
-console.log(lcm(123,276))
+var find_e = (p,q) => {
+    var phi = lcm(p - 1, q - 1)
+    for(var i = 2; i < phi; i ++){
+        if(gcd(i, phi) == 1){
+            return i
+        }
+    }
+}
+
+var prueba = find_n()
+var e = find_e(prueba[0], prueba[1])
+console.log(prueba)
+console.log(e)
+console.log(gcd(e, prueba[0] - 1))
+console.log(gcd(e, prueba[1] - 1))
